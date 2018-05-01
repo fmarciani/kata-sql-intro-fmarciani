3456a34468f0b518d4c7ef4ea177ded302c7d54a
Francesca Marciani <fmarciani@gmail.com>
2018-04-30 19:41:31 -0500

    Added interface IProductRepository.

---
+++ SqlIntro/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SqlIntro
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        void DeleteProduct(int id);
+        void UpdateProduct(Product prod);
+        void InsertProduct(Product prod);
+    }
+}
