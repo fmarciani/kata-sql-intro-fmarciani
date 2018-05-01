aa2d2979efc3db077faa67764cd783bad3fefc39
Francesca Marciani <fmarciani@gmail.com>
2018-04-30 19:44:15 -0500

    Added DapperProductRepository and associated code.

---
+++ SqlIntro/DapperProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SqlIntro
+{
+    class DapperProductRepo : IProductRepository
+    {
+        private readonly string _connectionString;
+
+        public DapperProductRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT ProductId as Id, Name from product");
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("DELETE from Product WHERE ProductId = @id", new { id });
+            }
+        }
+
+        public void UpdateProduct(Product prod)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("UPDATE product SET Name = @Name WHERE Id = @id", new { prod });
+            }
+        }
+
+        public void InsertProduct(Product prod)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                conn.Execute("INSERT INTO product (Name) VALUES(@Name)", new { prod });
+            }
+        }
+        //THE ONLY THING THAT CHANGES IS THE WAY WE QUERY OUR DATABASE
+    }
+}
