6970aa8c3bb2adb24c33f8371fdc13b4fa86233a
Francesca Marciani <fmarciani@gmail.com>
2018-05-01 12:46:23 -0500

    Changed string syntactic error in DapperProductRepository.

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -7,16 +7,24 @@ namespace SqlIntro
         static void Main(string[] args)
         {
             var connectionString = ""; //get connectionString format from connectionstrings.com and change to match your database
-            var repo = new ProductRepository(connectionString);
-
+            var repo = new DapperProductRepo(connectionString);
+            /*
             foreach (var prod in repo.GetProducts())
             {
                 Console.WriteLine("Product Name:" + prod.Name);
+            }*/
+
+            foreach (var prod in repo.GetProductsWithReview())
+            {
+                Console.WriteLine("\nProduct Name:" + prod.Name + "\nProduct Review:" + prod.Comments);
             }
+            /*
+            foreach (var prod in repo.GetProductsAndReviews())
+            {
+                Console.WriteLine("\nProduct Name:" + prod.Name, "\nProduct Review (if available):" + prod.Comments);
+            }*/
 
             Console.ReadLine();
         }
-
-       
     }
 }
